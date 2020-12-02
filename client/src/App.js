175b3432e2a69975e70fdee8359b9646a04be0ed
dlucarelli <dyllucar@gmail.com>
2020-12-02 09:33:29 -0600

    updates app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/savedBooks";
+import Nav from "./Components/Nav/Nav";
+import Header from "./Components/Header/Header";
+import Search from "./pages/index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+        <Nav />
+        <Header />
+        {/* <Books /> */}
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/searched" component={Search} />
+          <Route exaxt path="/saved" component={Saved} />
+        </Switch>
+      </div>
+      </Router>
+      
+        
+        
+    );
+  }
 }
 
 export default App;
